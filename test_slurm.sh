58d7ebd1911861cc223f35379db4ec15bd4c5a1f
Leonard Knirsch <leonard.knirsch@meteoswiss.ch>
2024-04-18 15:10:24 +0200

    extend test script

--- test_slurm.sh
+++ test_slurm.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -l
+
+# --ntasks-per-node and --gres=gpu must be the same number
+# you get a total of nodes*ntasks GPUs
+
 #SBATCH --job-name=pl_ddp_test
-#SBATCH --output=lightning_logs/pl_ddp_test.out
-#SBATCH --error=lightning_logs/pl_ddp_test.err
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --output=lightning_logs/%jpl_ddp_test.out
+#SBATCH --error=lightning_logs/%jpl_ddp_test.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:2
 #SBATCH --time=00:10:00
 #SBATCH --partition=normal
 
