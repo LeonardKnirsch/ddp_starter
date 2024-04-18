266552300321929776c83cadec64b38ffdbb5f86
Verena Bessenbacher <verena.bessenbacher@meteoswiss.ch>
2024-04-18 16:41:30 +0200

    change log files name

---
+++ test_slurm_ray.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+# --ntasks-per-node and --gres=gpu must be the same number
+# you get a total of nodes*ntasks GPUs
+
+#SBATCH --job-name=pl_ddp_test
+#SBATCH --output=lightning_logs/%jpl_ddp_test_ray.out
+#SBATCH --error=lightning_logs/%jpl_ddp_test_ray.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:2
+#SBATCH --time=00:10:00
+#SBATCH --partition=normal
+
+# Load necessary modules
+conda activate rayenv
+
+# Run the script
+#srun -ul python test_ddp.py
+srun -ul python test_imageclassifier_ray.py
